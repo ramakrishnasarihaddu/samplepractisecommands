3ea70f949339f36b5179b37bb34202efbc484168
root <root@ip-172-31-9-228.ap-south-1.compute.internal>
2024-02-13 13:47:45 +0000

    javacode written

---
+++ DataTils.java
@@ -0,0 +1,11 @@
+publis void main(String S){
+        Public static void main(String args[]){
+                int a=10;
+                int b=20;
+                int c=a+b;
+                System.out.println(c);
+
+
+        }
+}
+
